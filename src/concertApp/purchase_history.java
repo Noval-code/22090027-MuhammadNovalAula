6a1c3015bd00ffe5214d99ea61f6ee03619c65bc
Noval-code <novalaula486@gmail.com>
2023-10-09 13:24:07 +0700

    new feature done

--- src/concertApp/purchase_history.java
+++ src/concertApp/purchase_history.java
@@ -9,5 +9,22 @@ package concertApp;
  * @author muhammad noval aula
  */
 public class purchase_history {
+    private int historyId;
+    private int userId;
+    private int pembelianId;
+    
+    public void setHistoryId(int historyId){
+        this.historyId = historyId;
+    }
+    public int getHistoryId(){
+        return historyId;
+    }
+    
+    public void setPembelianId(purchase purchase){
+        this.pembelianId = purchase.getPembelianId();
+    }
+    public int getPembelianId(){
+        return pembelianId;
+    }
     
 }
